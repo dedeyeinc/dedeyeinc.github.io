9b7d1781a9d70bde45a682680df8ab3bd69bc4c8
Mathias Villalobos <mathiasv@dedeyeinc.com>
2024-02-10 10:13:10 -0800

    font test

--- 10_3DText/exercise/vite.config.js
+++ 10_3DText/exercise/vite.config.js
@@ -1,3 +1,4 @@
+
 export default {
     root: './',
     publicDir: '../static/',
@@ -10,7 +11,7 @@ export default {
     build:
     {
         outDir: 'D:/DEI_depot/dedeyeinc.github.io/ThreeJS', // Output in the dist/ folder
-        emptyOutDir: false, // Empty the folder first
+        emptyOutDir: true, // Empty the folder first
         sourcemap: true // Add sourcemap
     },
 }
